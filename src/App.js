1867170804c7355230abf4766585aa6cc2efd7bd
Jayaram Nair <jayaramsivaramannair@gmail.com>
2022-04-14 04:48:06 -0400

    Added the code to toggle and show list of patterns in Controls component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, {useState} from 'react';
 import Controls from './components/Controls'
 
 function App() {
+  const [showPatterns, setShowPatterns] = useState(false)
   return (
     <div className="App">
       <h1>Conway's Game of Life</h1>
-      <Controls />
+      <Controls
+        showPatterns = {showPatterns}
+        setShowPatterns = {setShowPatterns}
+      />
     </div>
   );
 }
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,25 +1,44 @@
 import React from 'react'
 
-const Controls = () => {
+const Controls = ({showPatterns, setShowPatterns}) => {
+  const togglePatterns = () => {
+    setShowPatterns(!showPatterns)
+  }
   return (
-    <div className="controls">
-      <div className="display">
-        Generation
-        <p>0</p>
+      <div>
+      {
+        (!showPatterns) ?
+        <div className="controls">
+          <div className="display">
+            Generation
+            <p>0</p>
+          </div>
+          <div className="option">
+            Start/Pause/Resume
+          </div>
+          <div className="option">
+            Randomize
+          </div>
+          <div className="option">
+            Clear Board
+          </div>
+          <div className="option" onClick={togglePatterns}>
+            Patterns
+          </div>
+        </div>
+         : 
+        <div className="controls"> 
+          <div className="option">Glider Gun</div>
+          <div className="option">Pulsar</div>
+          <div className="option">Crazy Corners</div>
+          <div className="option">Pentadecathlon</div>
+          <div className="option">Baby Pulsar</div>
+          <div className="option">Load Pattern</div>
+          <div className="option">Maximum Density Still Life</div>
+          <div className="option" onClick={togglePatterns}>Back To Controls</div>
+        </div>
+      }
       </div>
-      <div className="option">
-        Start/Pause/Resume
-      </div>
-      <div className="option">
-        Randomize
-      </div>
-      <div className="option">
-        Clear Board
-      </div>
-      <div className="option">
-        Patterns
-      </div>
-    </div>
   )
 }
 
